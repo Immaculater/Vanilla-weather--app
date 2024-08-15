0bba71c41e70fa9eeae6af09a1e8ba4022f07178
Immaculater <immaculaterbayero@gmail.com>
2024-08-15 22:36:27 +0100

    added axios and more functionalities using Api's

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
+function refreshWeather(response) {
+  console.log(response.data);
+
+  let temperature = response.data.temperature.current;
+  let weatherElement = document.querySelector("#weather-temperature");
+  weatherElement.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function search(city) {
+  let apiKey = "1ea13fb3c286o84864f57t06508d1bba";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
   //console.log(searchFormInput.value);
 
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchFormInput.value;
+  search(searchFormInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchSubmit);
+
+search("Jos");
