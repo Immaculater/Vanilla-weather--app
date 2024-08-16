d9ddf53ef97875b338da8534d2c4d9e7be006b4f
Immaculater <immaculaterbayero@gmail.com>
2024-08-16 16:14:16 +0100

    added more weather functionalities using Api's

--- src/app.js
+++ src/app.js
@@ -1,12 +1,41 @@
 function refreshWeather(response) {
-  console.log(response.data);
+  //console.log(response.data);
 
   let temperature = response.data.temperature.current;
   let weatherElement = document.querySelector("#weather-temperature");
-  weatherElement.innerHTML = Math.round(temperature);
-
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  weatherElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatedDate(date); //`${date.getDay()} ${date.getHours()} : ${date.getMinutes()} `;
+}
+
+function formatedDate(date) {
+  let minuites = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minuites < 10) {
+    minuites = `0${minuites}`;
+  }
+  return `${day} ${hours}:${minuites}, `;
 }
 
 function search(city) {
@@ -19,7 +48,7 @@ function search(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  //console.log(searchFormInput.value);
+  console.log(searchFormInput.value);
 
   search(searchFormInput.value);
 }
