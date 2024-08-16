c50b4a18cf8c9c076821e23ab4756bcd980690ff
Immaculater <immaculaterbayero@gmail.com>
2024-08-16 16:58:35 +0100

    worked on the weather icon

--- src/app.js
+++ src/app.js
@@ -9,13 +9,15 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
+  let iconElement = document.querySelector("#icon"); 
+ 
   weatherElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatedDate(date); //`${date.getDay()} ${date.getHours()} : ${date.getMinutes()} `;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temp-icon">`;
 }
 
 function formatedDate(date) {
